fb4810126bef42ed62283534114e70612b6d51a9
Chester <chesterdeveloper2000@gmail.com>
2020-07-04 09:12:06 +0800

    added tick animation, still blocky and jittery tho

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const durationInput = document.querySelector("#duration")
 const startBtn = document.querySelector("#start")
 const pauseBtn = document.querySelector("#pause")
+const circle = document.querySelector("circle")
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI
+circle.setAttribute("stroke-dasharray", perimeter)
+
+let currentOffset = 0
 
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
     onStart() {
         console.log("timer started!!")
     }, 
     onTick() {
-        console.log("timer just ticked!")
+        circle.setAttribute("stroke-dashoffset", currentOffset)
+        currentOffset = currentOffset - 50
     },
     onComplete(){
         console.log("timer completed!")

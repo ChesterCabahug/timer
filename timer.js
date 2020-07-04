e11dec9150c09875a73271f7699fae170a4a550f
Chester <chesterdeveloper2000@gmail.com>
2020-07-04 09:28:00 +0800

    fixed circle animation, it now follows the timer

--- timer.js
+++ timer.js
@@ -15,10 +15,10 @@ class Timer {
 
     start = () => {
         if(this.onStart){
-            this.onStart()
+            this.onStart(this.timeRemaining)
         }
         this.tick()
-        this.interval = setInterval(this.tick, 1000)
+        this.interval = setInterval(this.tick, 10)
     }
 
     pause = () => {
@@ -33,9 +33,9 @@ class Timer {
             }
         }
         else {
-            this.timeRemaining = this.timeRemaining - 1
+            this.timeRemaining = this.timeRemaining - .01
             if (this.onTick) {
-                this.onTick()
+                this.onTick(this.timeRemaining)
             }
         }
     }
@@ -45,7 +45,7 @@ class Timer {
     }
 
     set timeRemaining(time) {
-        this.durationInput.value = time
+        this.durationInput.value = time.toFixed(2)
     }
 
 }
